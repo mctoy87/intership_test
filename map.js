b1905cb40ffb0f3e37c387fcd81ad8cb429a30d8
mctoy87 <mctoy87@mail.ru>
2023-05-20 12:45:15 +0200

    connected map API, added hover, fix some style problems

---
+++ map.js
@@ -0,0 +1,29 @@
+/*инициализируем функцию загрузки местоположения по требованию*/
+function init() {
+  let myMap = new ymaps.Map('map-test', {
+    center: [59.924159318995876,30.24113793946679],
+    zoom: 14,
+  });
+  myGeoObject = new ymaps.GeoObject ({
+    geometry: {
+        type: "Point", // тип геометрии - точка
+        coordinates: [59.924159318995876,30.24113793946679] // координаты точки
+    },
+    // Свойства.
+    properties: {
+      // Контент метки.
+      iconContent: 'Я здесь',
+    },
+  }, {
+      // Опции.
+      // Иконка метки будет растягиваться под размер ее содержимого.
+      preset: 'islands#blackStretchyIcon',
+      // Метку можно перемещать.
+      draggable: true
+  });
+  // Размещение геообъекта на карте.
+  myMap.geoObjects.add(myGeoObject); 
+}
+
+ymaps.ready(init);
+
